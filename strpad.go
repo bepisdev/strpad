a7ae66e89349ed12eb0fa3109b92dde292c6be35
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-09-09 12:22:43 +1000

    stub Padding functions

---
+++ strpad.go
@@ -0,0 +1,19 @@
+package strpad
+
+/// Pad with one space to both sides
+func Pad(string inpstr, N int) string {}
+
+/// Pad with one space to the left
+func PadLeft(string inpstr, N int) string {}
+
+/// Pad with one space to the right
+func PadRight(string inpstr, N int) string {}
+
+/// Pad with N spaces to both sides
+func PadN(string inpstr, N int) string {}
+
+/// Pad with N spaces to the left
+func PadNLeft(string inpstr, N int) string {}
+
+/// Pad with N spaces to the right
+func PadNRight(string inpstr, N int) string {}
