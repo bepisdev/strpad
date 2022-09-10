3966d3fd374ae817fa24cfadc91205bd01d60122
Josh Burns <joshyburnss@gmail.com>
2022-09-10 12:25:36 +1000

    add iterator style functions

--- strpad.go
+++ strpad.go
@@ -1,6 +1,9 @@
 package strpad
 
-import "fmt"
+import (
+    "fmt"
+    "strings"
+)
 
 // Pad with one space to both sides
 func Pad(inpstr string) string {
@@ -15,4 +18,22 @@ func PadLeft(inpstr string) string {
 // Pad with one space to the right
 func PadRight(inpstr string) string {
 	return fmt.Sprintf("%s ", inpstr)
+}
+
+// Pad with N spaces to both sides
+func PadN(inpstr string, N int)  string {
+    _nspaces := strings.Repeat(" ")
+    return fmt.Sptrintf("%s%s%s", _nspaces, inpstr, _nspaces)
+}
+
+// Pad with N spaces to the left
+func PadNLeft(inpstr string, N int)  string {
+    _nspaces := strings.Repeat(" ")
+    return fmt.Sptrintf("%s%s", _nspaces, inpstr)
+}
+
+// Pad with N spaces to the right
+func PadNRight(inpstr string, N int)  string {
+    _nspaces := strings.Repeat(" ")
+    return fmt.Sptrintf("%s%s", inpstr, _nspaces)
 }
