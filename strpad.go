b6bf741efa14531f2ae9c3aafda194ff17dcc305
Josh Burns <joshyburnss@gmail.com>
2022-09-10 10:07:15 +1000

    implement padding functions

--- strpad.go
+++ strpad.go
@@ -1,19 +1,18 @@
 package strpad
 
-/// Pad with one space to both sides
-func Pad(string inpstr, N int) string {}
+import "fmt"
 
-/// Pad with one space to the left
-func PadLeft(string inpstr, N int) string {}
+// Pad with one space to both sides
+func Pad(string inpstr) string {
+    return fmt.Sprintf(" %s ", inpstr)
+}
 
-/// Pad with one space to the right
-func PadRight(string inpstr, N int) string {}
+// Pad with one space to the left
+func PadLeft(string inpstr, N int) string {
+    return fmt.Sprintf(" %s", inpstr)
+}
 
-/// Pad with N spaces to both sides
-func PadN(string inpstr, N int) string {}
-
-/// Pad with N spaces to the left
-func PadNLeft(string inpstr, N int) string {}
-
-/// Pad with N spaces to the right
-func PadNRight(string inpstr, N int) string {}
+// Pad with one space to the right
+func PadRight(string inpstr, N int) string {
+    return fmt.Sprintf("%s ", inpstr)
+}
