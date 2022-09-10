6a3fd6b35eaa89de15ed04e57df09d1cfcf3c0ca
Josh Burns <joshyburnss@gmail.com>
2022-09-10 11:52:10 +1000

    format strpad.go

--- strpad.go
+++ strpad.go
@@ -4,15 +4,15 @@ import "fmt"
 
 // Pad with one space to both sides
 func Pad(inpstr string) string {
-    return fmt.Sprintf(" %s ", inpstr)
+	return fmt.Sprintf(" %s ", inpstr)
 }
 
 // Pad with one space to the left
 func PadLeft(inpstr string) string {
-    return fmt.Sprintf(" %s", inpstr)
+	return fmt.Sprintf(" %s", inpstr)
 }
 
 // Pad with one space to the right
 func PadRight(inpstr string) string {
-    return fmt.Sprintf("%s ", inpstr)
+	return fmt.Sprintf("%s ", inpstr)
 }
