250f44434836fc73d9d69d1e04364445b77191cb
Josh Burns <joshyburnss@gmail.com>
2022-09-10 10:37:26 +1000

    fix params

--- strpad.go
+++ strpad.go
@@ -3,16 +3,16 @@ package strpad
 import "fmt"
 
 // Pad with one space to both sides
-func Pad(string inpstr) string {
+func Pad(inpstr string) string {
     return fmt.Sprintf(" %s ", inpstr)
 }
 
 // Pad with one space to the left
-func PadLeft(string inpstr, N int) string {
+func PadLeft(inpstr string) string {
     return fmt.Sprintf(" %s", inpstr)
 }
 
 // Pad with one space to the right
-func PadRight(string inpstr, N int) string {
+func PadRight(inpstr string) string {
     return fmt.Sprintf("%s ", inpstr)
 }
