0b340247ba71f388ae23a0d748690906fcf63970
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-10-07 06:59:22 +1100

    cleanup extra whitespace

--- strpad.go
+++ strpad.go
@@ -21,52 +21,52 @@ func PadRight(inpstr string) string {
 }
 
 // Pad with N spaces to both sides
-func PadN(inpstr string, N int)  string {
+func PadN(inpstr string, N int) string {
     _nspaces := strings.Repeat(" ", N)
     return fmt.Sprintf("%s%s%s", _nspaces, inpstr, _nspaces)
 }
 
 // Pad with N spaces to the left
-func PadNLeft(inpstr string, N int)  string {
+func PadNLeft(inpstr string, N int) string {
     _nspaces := strings.Repeat(" ", N)
     return fmt.Sprintf("%s%s", _nspaces, inpstr)
 }
 
 // Pad with N spaces to the right
-func PadNRight(inpstr string, N int)  string {
+func PadNRight(inpstr string, N int) string {
     _nspaces := strings.Repeat(" ", N)
     return fmt.Sprintf("%s%s", inpstr, _nspaces)
 }
 
 // Pad with X symbol to both sides
-func PadSymbol(inpstr string, X string)  string {
+func PadSymbol(inpstr string, X string) string {
     return fmt.Sprintf("%s%s%s", X, inpstr, X)
 }
 
 // Pad with X symbol to the left
-func PadSymbolLeft(inpstr string, X string)  string {
+func PadSymbolLeft(inpstr string, X string) string {
     return fmt.Sprintf("%s%s", X, inpstr)
 }
 
 // Pad with X symbol to the right
-func PadSymbolRight(inpstr string, X string)  string {
+func PadSymbolRight(inpstr string, X string) string {
     return fmt.Sprintf("%s%s", inpstr, X)
 }
 
 // Pad with N number of X symbol to both sides
-func PadNSymbol(inpstr string, X string, N int)  string {
+func PadNSymbol(inpstr string, X string, N int) string {
     _nsymbols := strings.Repeat(X, N)
     return fmt.Sprintf("%s%s%s", _nsymbols, inpstr, _nsymbols)
 }
 
 // Pad with N number of X symbol to the right
-func PadNSymbolRight(inpstr string, X string, N int)  string {
+func PadNSymbolRight(inpstr string, X string, N int) string {
     _nsymbols := strings.Repeat(X, N)
     return fmt.Sprintf("%s%s", inpstr, _nsymbols)
 }
 
 // Pad with N number of X symbol to the left
-func PadNSymbolRight(inpstr string, X string, N int)  string {
+func PadNSymbolRight(inpstr string, X string, N int) string {
     _nsymbols := strings.Repeat(X, N)
     return fmt.Sprintf("%s%s", _nsymbols, inpstr)
 }
